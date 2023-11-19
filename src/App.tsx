a0c3578488d86499080c74b65081cd11ca6c6665
Érus Ferreira <erusferreira@gmail.com>
2023-11-19 17:04:44 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+
+import Login from "./pages/Login";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
+}
+
+export default App;
