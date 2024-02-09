fa856323196aecc34329074cd314b93b70b62b18
Érus Ferreira <erusferreira@gmail.com>
2024-02-09 18:20:10 -0300

    Stretched top menu

--- src/pages/catalog/Main/index.tsx
+++ src/pages/catalog/Main/index.tsx
@@ -44,7 +44,7 @@ export default function Main(): JSX.Element {
   return (
     <>
       <nav className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="mx-auto px-2">
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
